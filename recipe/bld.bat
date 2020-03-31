3085106ff360cc555ec837cff605d9369cf2b1ba
Anthony Scopatz <scopatz@gmail.com>
2020-03-31 11:48:27 -0500

    windows full install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,21 @@
-@echo on
+setlocal EnableDelayedExpansion
 
-nmake /f msvc.mak LIBPNGDIR=%LIBRARY_LIB% ZLIBDIR=%LIBRARY_LIB%
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+:: Configure using the CMakeFiles
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      ..
 if errorlevel 1 exit 1
 
-copy .\\jbig2dec.exe %LIBRARY_BIN%\\jbig2dec.exe
+:: Build!
+nmake
 if errorlevel 1 exit 1
 
-dir .
+:: Install!
+nmake install
+if errorlevel 1 exit 1
